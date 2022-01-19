279683f3c12a05df0ba96276e2093a137d2e706b
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-01-19 07:41:53 +0200

    feat(navabr): changed body color in Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "./Navbar";
 const Layout: React.FC = ({ children }) => {
   return (
-    <div>
+    <div className="bg-background">
       <Navbar />
       {children}
     </div>
