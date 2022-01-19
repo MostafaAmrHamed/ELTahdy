5ee530704c2aaba059322e3ce905223cf65520f4
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-01-19 07:40:56 +0200

    {add}: customized font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#f8faff",
+        component: "#e1e8fe",
+        color: {
+          1: "#ffffff",
+          2: "#eef3ff",
+          3: "#a9a7c9",
+          4: "#6159e6",
+        },
+        cancel: "#f96879",
+        true: "#008000",
+      },
+      fontFamily: {
+        body: ["Markazi"],
+      },
+    },
   },
   plugins: [],
-}
+};
