b637065b567a23e9017a6f68053ca00ef984246b
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-01-30 14:59:34 +0200

    fix(footer): center footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div
-      className="grid place-content-center md:grid-cols-2 lg:grid-cols-3 items-center bg-color-4 text-color-1 font-medium mt-5 py-2 pl-10 w-full"
+      className="grid md:grid-cols-2 lg:grid-cols-3 items-center text-center bg-color-4 text-color-1 font-medium mt-5 py-2 w-full"
       style={{ direction: "ltr" }}
     >
       <p>&copy;2022 EL-Tahdy. All rights reserved</p>
