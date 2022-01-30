5fdec25ffa9303f4325f97c6b3671bf9b2099763
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-01-30 16:54:51 +0200

    feat(add-student): header and responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="bg-color-4 py-2 w-fit text-center mx-auto">
-      <h1> إضافة طالب </h1>
+    <div className="bg-color-4 py-2 w-[300px] md:w-[350px] md:py-3 text-center rounded-md mx-auto mt-2">
+      <h1 className="text-color-2 font-semibold text-3xl md:text-4xl">
+        إضـافة طـالب
+      </h1>
     </div>
   );
 }
