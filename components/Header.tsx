714a3940039f380ab8e48a011af0a6ef9438b59c
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-01-30 15:27:16 +0200

    feat(add-student): init

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="bg-color-4 py-2 w-fit text-center mx-auto">
+      <h1> إضافة طالب </h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ pages/addStudent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "../components/Header";
+function addStudent() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default addStudent;
