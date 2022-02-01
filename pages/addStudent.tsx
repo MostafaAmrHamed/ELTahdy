0ae15421f8e48ff0399cd4070e6df08c0710385f
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-01 18:53:58 +0200

    feat(add-student): added validation for  inputs

--- pages/addStudent.tsx
+++ pages/addStudent.tsx
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
-function addStudent() {
-  const [student, setStudent] = useState({
+import Validate from "../components/Validation";
+import {
+  nameValidate,
+  phoneValidate,
+  passwordValidate,
+  yearValidate,
+} from "../logical/validation";
+import { studentData } from "../types";
+const addStudent = () => {
+  const [validate, setValidate] = useState(false);
+  const [student, setStudent] = useState<studentData>({
     name: "",
     password: "",
     phone: "",
@@ -10,11 +19,22 @@ function addStudent() {
   });
   const resetData = () => {
     setStudent({ name: "", password: "", phone: "", phone2: "", year: "" });
+    setValidate(false);
   };
+
   const handleSubmit = (event: any) => {
     event.preventDefault();
-    resetData();
-    console.log(student);
+    if (
+      nameValidate(student.name).validate ||
+      phoneValidate(student.phone).validate ||
+      phoneValidate(student.phone2).validate
+    ) {
+      setValidate(true);
+    } else {
+      setValidate(false);
+      console.log(student);
+      resetData();
+    }
   };
   return (
     <div className="container mx-auto">
@@ -33,6 +53,14 @@ function addStudent() {
                 setStudent({ ...student, name: e.target.value });
               }}
             />
+            {validate && (
+              <div className="mt-2 col-start-2 col-span-3 text-center md:text-right md:pr-2">
+                <Validate
+                  message={nameValidate(student.name).message}
+                  validation={nameValidate(student.name).validate}
+                />
+              </div>
+            )}
           </div>
           <div className="mb-5 border-b-2 border-color-2 mx-5 pb-5 md:grid md:grid-cols-4 items-center text-center ">
             <label className="text-color-4 text-2xl font-semibold">
@@ -46,36 +74,56 @@ function addStudent() {
                 setStudent({ ...student, password: e.target.value });
               }}
             />
+            {validate && (
+              <div className="mt-2 col-start-2 col-span-3 text-center md:text-right md:pr-2">
+                <Validate
+                  message={passwordValidate(student.password).message}
+                  validation={passwordValidate(student.password).validate}
+                />
+              </div>
+            )}
           </div>
           <div className="mb-5 border-b-2 border-color-2 mx-5 pb-5 md:grid md:grid-cols-4 items-center text-center ">
             <label className="text-color-4 text-2xl font-semibold">
               رقم الهاتف
             </label>
             <input
-              type="text"
+              type="number"
               className="w-11/12 md:w-auto md:col-span-3 py-2 px-2 mt-3 md:mt-0 md:p-2 text-xl rounded-md focus:outline-none focus:ring-2 focus:ring-color-4 shadow-sm"
               value={student.phone}
               onChange={(e) => {
                 setStudent({ ...student, phone: e.target.value });
               }}
-              pattern="^[0-9]{12}$"
-              required
             />
+            {validate && (
+              <div className="mt-2 col-start-2 col-span-3 text-center md:text-right md:pr-2">
+                <Validate
+                  message={phoneValidate(student.phone).message}
+                  validation={phoneValidate(student.phone).validate}
+                />
+              </div>
+            )}
           </div>
           <div className="mb-5 border-b-2 border-color-2 mx-5 pb-5 md:grid md:grid-cols-4 items-center text-center">
             <label className="text-color-4 text-2xl font-semibold">
               رقم هاتف الأب
             </label>
             <input
-              type="text"
+              type="number"
               className="w-11/12 md:w-auto md:col-span-3 py-2 px-2 mt-3 md:mt-0 md:p-2 text-xl rounded-md focus:outline-none focus:ring-2 focus:ring-color-4 shadow-sm"
               value={student.phone2}
               onChange={(e) => {
                 setStudent({ ...student, phone2: e.target.value });
               }}
-              pattern="[0-9]+"
-              required
             />
+            {validate && (
+              <div className="mt-2 col-start-2 col-span-3 text-center md:text-right md:pr-2">
+                <Validate
+                  message={phoneValidate(student.phone2).message}
+                  validation={phoneValidate(student.phone2).validate}
+                />
+              </div>
+            )}
           </div>
           <div className="mb-5 mx-5 pb-5 grid gap-2 md:gap-0 md:grid-cols-4 items-center place-content-center text-center">
             <label className="text-color-4 text-2xl font-semibold">الصف </label>
@@ -92,6 +140,14 @@ function addStudent() {
               <option value="ثانية ادبي">ثانية ادبي</option>
               <option value="ثالثة">ثالثة</option>
             </select>
+            {validate && (
+              <div className="mt-2 col-start-2 col-span-3 text-center md:text-right md:pr-2">
+                <Validate
+                  message={yearValidate(student.year).message}
+                  validation={yearValidate(student.year).validate}
+                />
+              </div>
+            )}
           </div>
           <div className="flex flex-col mt-5 items-center md:flex-row gap-5 justify-center">
             <button
@@ -112,6 +168,6 @@ function addStudent() {
       </div>
     </div>
   );
-}
+};
 
 export default addStudent;
