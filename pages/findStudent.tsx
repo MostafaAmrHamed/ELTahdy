aad5ffc727a677dcc1935117be40556355097cec
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-16 17:06:23 +0200

    feat(find-student): filtering data

--- pages/findStudent.tsx
+++ pages/findStudent.tsx
@@ -2,33 +2,54 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import StudentCard from "../components/StudentCard";
 import Pagination from "../components/Pagination";
+import { studentSearchData } from "../types";
 const findStudent = () => {
   const [search, setSearch] = useState({ year: "", studentName: "" });
   const [currentPage, setCurrentPage] = useState(1);
-  const [filterData, setFilterData] = useState({});
+  const [filterData, setFilterData] = useState<studentSearchData[]>([]);
   const handleSubmit = () => {
     console.log(search.studentName + " " + search.year);
+    setFilterData([]);
+    for (let student of fakeData) {
+      if (student.studentName.includes(search.studentName)) {
+        setFilterData([...filterData, student]);
+        console.log(student.studentName + " Inside");
+      }
+    }
   };
-  const fakeData = [
-    { id: 1, name: "Mostafa Amr", year: "اولى" },
-    { id: 2, name: "Mostafa Amr", year: "اولى" },
-    { id: 3, name: "Mostafa Amr", year: "اولى" },
-    { id: 4, name: "Mostafa Amr", year: "اولى" },
-    { id: 5, name: "Mostafa Amr", year: "اولى" },
-    { id: 6, name: "Mohamed Amr", year: "2nd" },
-    { id: 7, name: "Mostafa Amr", year: "اولى" },
-    { id: 8, name: "Mohamed Amr", year: "2nd" },
-    { id: 9, name: "Mohamed Amr", year: "2nd" },
-    { id: 10, name: "Mohamed Amr", year: "2nd" },
-    { id: 11, name: "Menna Amr", year: "3rd" },
-    { id: 12, name: "Menna Amr", year: "3rd" },
-    { id: 13, name: "Menna Amr", year: "3rd" },
-    { id: 14, name: "Menna Amr", year: "3rd" },
-    { id: 15, name: "Menna Amr", year: "3rd" },
-    { id: 16, name: "Menna Amr", year: "3rd" },
+  const yearOnChange = (e: any) => {
+    setSearch({ ...search, year: e.target.value });
+    setFilterData([]);
+    for (let student of fakeData) {
+      if (student.studentName.includes(search.studentName)) {
+        setFilterData([...filterData, student]);
+        console.log(student.studentName);
+      }
+    }
+  };
+  const fakeData: studentSearchData[] = [
+    { id: 1, studentName: "Mostafa Amr", year: "اولى" },
+    { id: 2, studentName: "Mostafa Amr", year: "اولى" },
+    { id: 3, studentName: "Mostafa Amr", year: "اولى" },
+    { id: 4, studentName: "Mostafa Amr", year: "اولى" },
+    { id: 5, studentName: "Mostafa Amr", year: "اولى" },
+    { id: 6, studentName: "Mohamed Amr", year: "2nd" },
+    { id: 7, studentName: "Mostafa Amr", year: "اولى" },
+    { id: 8, studentName: "Mohamed Amr", year: "2nd" },
+    { id: 9, studentName: "Mohamed Amr", year: "2nd" },
+    { id: 10, studentName: "Mohamed Amr", year: "2nd" },
+    { id: 11, studentName: "Menna Amr", year: "3rd" },
+    { id: 12, studentName: "Menna Amr", year: "3rd" },
+    { id: 13, studentName: "Menna Amr", year: "3rd" },
+    { id: 14, studentName: "Menna Amr", year: "3rd" },
+    { id: 15, studentName: "Menna Amr", year: "3rd" },
+    { id: 16, studentName: "Menna Amr", year: "3rd" },
   ];
 
-  const datatoShow = fakeData.slice((currentPage - 1) * 5, currentPage * 5);
+  const datatoShow =
+    filterData.length === 0
+      ? filterData.slice((currentPage - 1) * 5, currentPage * 5)
+      : fakeData.slice((currentPage - 1) * 5, currentPage * 5);
   return (
     <div className="mx-auto">
       <Header title={"إيجاد طالب"} />
@@ -36,9 +57,7 @@ const findStudent = () => {
         <select
           className="py-2 pr-5 rounded-full w-full md:w-[200px] shadow-sm focus:outline-none focus:ring-2 focus:ring-color-4"
           value={search.year}
-          onChange={(e) => {
-            setSearch({ ...search, year: e.target.value });
-          }}
+          onChange={yearOnChange}
         >
           <option value="">الصف</option>
           <option value="اولى">اولى</option>
@@ -72,7 +91,7 @@ const findStudent = () => {
 
           <h1> الصـف</h1>
         </div>
-        {search.studentName &&
+        {/* {search.studentName &&
           fakeData.map((student, index) => {
             if (student.name.includes(search.studentName)) {
               return (
@@ -97,12 +116,22 @@ const findStudent = () => {
                 />
               );
             }
-          })}
-        <div className=" md:h-[500px]">
-          {datatoShow.map((student, index) => {
+          })} */}
+        <div className="md:h-[500px]">
+          {/* {datatoShow.map((student, index) => {
             return (
               <StudentCard
                 studentName={student.name}
+                year={student.year}
+                id={student.id}
+                key={index}
+              />
+            );
+          })} */}
+          {datatoShow.map((student, index) => {
+            return (
+              <StudentCard
+                studentName={student.studentName}
                 year={student.year}
                 id={student.id}
                 key={index}
